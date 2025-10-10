29b17f00e726cc4a3fa0fbfb83a1f95320710d7a
Dmitry-Lipinskiy <Dimalipa@mail.ru>
2025-10-10 23:32:54 +0300

    фильтрация и вывод карточек на страницу

--- js/viewAll.js
+++ js/viewAll.js
@@ -1,6 +1,5 @@
-const search = () => {
-  const input = document.querySelector(".search-block > input");
-  const searchBtn = document.querySelector(".search-block > button");
+const viewAll = () => {
+  const viewAllBtn = document.querySelector(".more");
 
   const renderGoods = (goods) => {
     const goodsContainer = document.querySelector(".long-goods-list");
@@ -37,27 +36,21 @@ const search = () => {
     });
   };
 
-  const getData = (value) => {
+  const getData = () => {
     fetch("/db/db.json")
       .then((res) => res.json())
       .then((data) => {
-        const array = data.filter((good) =>
-          good.name.toLowerCase().includes(value.toLowerCase())
-        );
-
-        localStorage.setItem("goods", JSON.stringify(array));
+        localStorage.setItem("goods", JSON.stringify(data));
 
         if (window.location.pathname !== "/goods.html") {
           window.location.href = "/goods.html";
         } else {
-          renderGoods(array);
+          renderGoods(data);
         }
       });
   };
 
-  searchBtn.addEventListener("click", () => {
-    getData(input.value);
-  });
+  viewAllBtn.addEventListener("click", () => getData());
 };
 
-search();
+viewAll();
